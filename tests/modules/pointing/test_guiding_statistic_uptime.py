2cf60c8f10d82445458618de410f065203b5f862
GermanHydrogen <admin@germanhydrogen.org>
2023-10-24 12:45:36 +0200

    Fixed header comment in unit test

--- tests/modules/pointing/test_guiding_statistic_uptime.py
+++ tests/modules/pointing/test_guiding_statistic_uptime.py
@@ -14,7 +14,7 @@ def test_end_to_end():
 
     header = statistic.add_to_header(client, {})
 
-    assert header["GUIDING UPTIME"] == (100.0, "Percentage of exposure time the guiding was closed.")
+    assert header["GUIDING UPTIME"] == (100.0, "Time the guiding loop was closed [%]")
 
 
 def test_calc_uptime_percentage():

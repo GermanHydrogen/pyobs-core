8f779c18fec4d37eacbead064322a746d4b4a4a1
germanhydrogen <admin@germanhydrogen.org>
2024-02-19 12:12:24 +0100

    Added integration test for projected

---
+++ tests/integration/images/processors/test_offset_calculators.py
@@ -0,0 +1,31 @@
+from typing import Tuple
+
+import numpy as np
+import pytest
+
+from pyobs.images import Image
+from pyobs.images.meta import PixelOffsets
+from pyobs.images.processors.offsets import *
+
+
+@pytest.fixture()
+def test_images() -> Tuple[Image, Image]:
+    image = Image.from_file("new-image.fits")
+    data = image.data
+
+    ref_data = np.pad(data, (1, 0), mode='constant')
+    ref_data = ref_data[:-1]  # Prune at the end to keep the size the same
+
+    return image, Image(ref_data)
+
+
+async def test_projected(test_images: Tuple[Image, Image]) -> None:
+    ref_image, test_image = test_images
+    calculator = ProjectedOffsets()
+
+    await calculator(ref_image)
+    result = await calculator(test_image)
+
+    pixel_offset = result.get_meta(PixelOffsets)
+    np.testing.assert_almost_equal(pixel_offset.dy, 1.0, 0)
+    np.testing.assert_almost_equal(pixel_offset.dx, 0.0, 0)
